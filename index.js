baf7d75662682b98563971da750f6a4098a83adf
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-05-05 17:12:14 -0400

    Generate random image

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+// Gives random number between 1-6
 let randomNumber1 = ((Math.floor(Math.random() * 6)) + 1);
-console.log(randomNumber1);
+
+// String of different image file  
+let randomDiceImage = "dice" + randomNumber1 + ".png";
+let randomImageSource = "images/" + randomDiceImage;
+
+// Select first <img> which is at index number 0 and set to random image
+let image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+
+
+// Gives random number between 1-6
+let randomNumber2 = ((Math.floor(Math.random() * 6)) + 1);
+
+// String of different image file
+let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+// Select second <img> which is at index number 1 and set to random image
+let image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
