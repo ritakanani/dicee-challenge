ba247362b74cffacc47a7f54397ff3dabeab9e11
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-05-06 14:39:46 -0400

    if condition for Player wins

--- index.js
+++ index.js
@@ -20,3 +20,14 @@ let randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 // Select second <img> which is at index number 1 and set to random image
 let image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomImageSource2);
+
+
+
+// If condition for which player wins 
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "⛳️ Player 1 wins!";  
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "⛳️ Player 2 wins!";  
+} else {
+  document.querySelector("h1").innerHTML = "Draw!"
+}
